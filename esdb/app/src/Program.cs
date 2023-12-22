4679ea655ea9cdf1f3b935daa4ba606501eaa83b
ross.buggins <78215796+RossBugginsNHS@users.noreply.github.com>
2023-12-22 13:23:46 +0000

    console keys.

--- esdb/app/src/Program.cs
+++ esdb/app/src/Program.cs
@@ -1,58 +1,85 @@
 ﻿
+using System.Text;
 using System.Text.Json;
+using System.Text.Unicode;
 using EventStore.Client;
+
+var testDebug = new TestEvent("aasdasd", "sdfsdfsdf sdfsdf");
 
 var cts = new CancellationTokenSource();
 var cancellationToken = cts.Token;
 
-const string connectionString = "esdb://admin:changeit@node1:2113?tls=false&tlsVerifyCert=false";
+//const string connectionString = "esdb://admin:changeit@node1:2113?tls=false&tlsVerifyCert=false";
+const string connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
 
-const string streamName = "some-stream";
-const string eventType = "TestEvent";
+const string streamPrefix = "user-";
 
 var settings = EventStoreClientSettings.Create(connectionString);
 var client = new EventStoreClient(settings);
 
 Console.WriteLine("Hi!");
 
+
+
 var t = Task.Run(async () =>
 {
     while (!cancellationToken.IsCancellationRequested)
     {
-        Console.WriteLine("Writing event");
-        var evt = new TestEvent(Guid.NewGuid().ToString("N"), "I wrote my first event!");
-        var eventData = new EventData(
-            Uuid.NewUuid(),
-            eventType,
-            JsonSerializer.SerializeToUtf8Bytes(evt)
-        );
+        if (Console.KeyAvailable)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine("Enter user Id");
+            var id = Console.ReadLine();
+            var streamName = streamPrefix + id;
 
-        await client.AppendToStreamAsync(
-            streamName,
-            StreamState.Any,
-            new[] { eventData },
-            cancellationToken: cancellationToken
-        );
+            var eventData = key.Key switch
+            {
+            ConsoleKey.A => new EventData(
+                                    Uuid.NewUuid(),
+                                    "UserRegistered",
+                                    JsonSerializer.SerializeToUtf8Bytes(
+                                        new TestEvent(Guid.NewGuid().ToString("N"), "I wrote my first event!")
+                                )),
+            ConsoleKey.B => new EventData(
+                                    Uuid.NewUuid(),
+                                    "UserNameSet",
+                                    JsonSerializer.SerializeToUtf8Bytes(
+                                        new FirstNameSetEvent("SomeNameSet")
+                                )),
+                    
+            _ => null
+            };
 
-        Console.WriteLine("Written event");
-        await Task.Delay(1000);
+            if(eventData!=null)
+                await client.AppendToStreamAsync(
+                    streamName,
+                    StreamState.Any,
+                    new[] { eventData },
+                    cancellationToken: cancellationToken
+                );
+
+            Console.WriteLine("Written event");
+        }
+        else
+        {
+            await Task.Delay(50);
+        }
     }
 });
 
-var result = client.ReadStreamAsync(
-    Direction.Forwards,
-    streamName,
-    StreamPosition.Start,
-    cancellationToken: cancellationToken
-);
+var readStreamName = "$ce-user";
 
 
-await client.SubscribeToStreamAsync(streamName, FromStream.Start, async (s, e, c) =>
+await client.SubscribeToStreamAsync(
+    readStreamName, FromStream.Start, async (s, e, c) =>
 {
-    var te = JsonSerializer.Deserialize<TestEvent>(e.Event.Data.ToArray());
-    Console.WriteLine(e.Event.EventNumber + "\t" + e.Event.EventType + "\t" + te);
+    var evt = e.Event;
+    var d = evt.Data.ToArray();
+    var str = Encoding.UTF8.GetString(d);
+    var te = JsonSerializer.Deserialize<TestEvent>(d);
+    Console.WriteLine(evt.EventNumber + "\t" + evt.EventType + "\t" + te);
     await Task.Yield();
-});
+}, resolveLinkTos: true);
 
 await t;
 
--- esdb/app/src/TestEvent.cs
+++ esdb/app/src/TestEvent.cs
@@ -1,1 +1,10 @@
-public record TestEvent(string EntityId, string ImportantData);
+
+using System.Diagnostics;
+
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
+public record TestEvent(string EntityId, string ImportantData)
+{
+    public string DebuggerDisplay => "DbgDisp " + EntityId + " " + ImportantData;
+}
+
+public record FirstNameSetEvent(string FirstName);
